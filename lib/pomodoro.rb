796c53fe160a9eab197852ab4f79cd724dd9f084
Xavier Tô <xavier.to.01@gmail.com>
2010-04-22 22:54:10 -0400

    Added pomodoro status and it\'s change

--- lib/pomodoro.rb
+++ lib/pomodoro.rb
@@ -3,15 +3,18 @@ require 'RNotify'
 class Pomodoro
   attr_reader :task;
   attr_reader :length
+  attr_reader :status
   def initialize length, task_name
     @length = length
     @task_name = task_name
+    @status = 'new'
   end
   
 
   def start
+    @status = 'in progress'
     Notify.init("Pomodoro")
-    notification = Notify::Notification.new "Pomodoro started", "You now have "+@length.to_s+" minutes to complete \""+@task_name+"\""
+    notification = Notify::Notification.new "Pomodoro started", "You now have "+@length.to_s+" minutes to complete \""+@task_name+"\"",nil,nil
     notification.show
 
     1.upto(@length){|i|
@@ -20,6 +23,9 @@ class Pomodoro
       notification.show
     }
     
+    notification.update "Pomodoro finished", "Take a break"
+    notification.show
+    @status = 'finished'
     Notify.uninit
   end
 
--- spec/pomodoro_spec.rb
+++ spec/pomodoro_spec.rb
@@ -4,15 +4,34 @@ require 'lib/pomodoro.rb'
 
 describe "Pomodoro" do
 
-  it "should send a notification every minute" do
+  before :each do
     Kernel.stub!(:sleep)
+    @mock_notify = mock(Notify::Notification)
+    Notify::Notification.stub!(:new).and_return(@mock_notify)
+  end
 
-    mock_notify = mock(Notify::Notification)
-    Notify::Notification.stub!(:new).and_return(mock_notify)
-    mock_notify.should_receive(:show).exactly(6).times
-    mock_notify.should_receive(:update).exactly(5).times
+  it "should send a notification every minute" do
+    @mock_notify.should_receive(:show).exactly(7).times
+    @mock_notify.should_receive(:update).exactly(6).times
     pomodoro = Pomodoro.new 5,"Task name"
     pomodoro.start
   end
+  
+  it "should change status from 'new' to 'in progress' when it is started" do
+    pomodoro = Pomodoro.new 5,"Task name"
+    pomodoro.status.should == 'new'
+    Thread.new {pomodoro.start}
+    sleep 1
+    pomodoro.status.should == 'in progress'
+  end
 
+  it "should change status from 'in progress' to 'finished' once the pomodoro is over" do
+    @mock_notify.should_receive(:show).exactly(3).times
+    @mock_notify.should_receive(:update).exactly(:twice)
+    pomodoro = Pomodoro.new 1,"Task name"
+    pomodoro.status.should == 'new'
+    pomodoro.start
+    sleep 1 
+    pomodoro.status.should == 'finished'
+  end
 end
